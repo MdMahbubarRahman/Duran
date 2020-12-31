5773d64b32f64a2e2e618367691c4c3401f6c9b2
Md Mahbubar Rahman <mrah06@gmail.com>
2020-12-30 20:43:08 -0600

    packages and files are liked with the Duran module.

--- src/Duran.jl
+++ src/Duran.jl
@@ -1,5 +1,52 @@
+#=
+
+=#
+
 module Duran
 
+import JuMP
+import Compat
+using JuMP: @variable, @constraint, @objective, Model, optimizer_with_attributes
+using JSON
+using LinearAlgebra
+using Random
+using Distributed
+using Statistics
+using SparseArrays
+
+using MathOptInterface, Ipopt, Cbc
+const MOI = MathOptInterface
+const MOIU = MOI.Utilities
+
+# functions
+const SVF = MOI.SingleVariable
+const SAF = MOI.ScalarAffineFunction{Float64}
+const SQF = MOI.ScalarQuadraticFunction{Float64}
+const VECTOR = MOI.VectorOfVariables
+
+include("storage_types.jl")
+function Base.show(io::IO, opts::SolverOptions)
+    longest_field_name = maximum([length(string(fname)) for fname in fieldnames(SolverOptions)])+2
+    for name in fieldnames(SolverOptions)
+        sname = string(name)
+        pname = sname*repeat(" ", longest_field_name-length(sname))
+        if getfield(opts,name) === nothing
+            println(io, pname, ": NA")
+        else
+            println(io, pname, ": ", getfield(opts,name))
+        end
+    end
+end
+
+include("util.jl")
+include("oa_util.jl")
+include("OriginalProblem.jl")
+include("solver_attributes.jl")
+include("moi_wrapper.jl")
+include("storage_types.jl")
+
 greet() = print("Hello World!")
+
+
 
 end # module
