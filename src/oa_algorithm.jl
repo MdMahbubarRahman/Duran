fb4975ff8b7dac86a28534e07885534855f8b6aa
Md Mahbubar Rahman <mrah06@gmail.com>
2021-01-06 01:29:20 -0600

    oa algorithm updated and now it is complete.

--- src/oa_algorithm.jl
+++ src/oa_algorithm.jl
@@ -8,23 +8,27 @@ function solve_ref_nlp_model(model::MOI.AbstractOptimizer, oad::OAdata)
 """
 function solve_ref_nlp_model(model::MOI.AbstractOptimizer, oad::OAdata)
     if oad.milp_sol_available
+        vars = JuMP.all_variables(oad.ref_nlp_model)
         for i in 1:oad.ref_num_var
             if oad.ref_var_type[i] == :Int || oad.ref_var_type[i] ==:Bin
-                JuMP.set_lower_bound(oad.ref_nlp_x[i], oad.ref_mip_solution[i])
-                JuMP.set_upper_bound(oad.ref_nlp_x[i], oad.ref_mip_solution[i])
+                JuMP.delete_lower_bound(vars[i])
+                JuMP.delete_upper_bound(vars[i])
+                JuMP.set_lower_bound(vars[i], oad.ref_mip_solution[i])
+                JuMP.set_upper_bound(vars[i], oad.ref_mip_solution[i])
             end
         end
     end
 
     JuMP.set_optimizer(oad.ref_nlp_model, model.options.nlp_solver)
-    JuMP.optimize!(oad.ref_nlp_model)
-    #
-    if JuMP.termination_status(oad.ref_nlp_model) == :OPTIMAL || JuMP.termination_status(oad.ref_nlp_model) == :LOCALLY_SOLVED
+    JuMP.optimize!(oad.ref_nlp_model);
+    #the problem is here
+    if (JuMP.termination_status(oad.ref_nlp_model) == MOI.OPTIMAL) || (JuMP.termination_status(oad.ref_nlp_model) == MOI.LOCALLY_SOLVED)
         @info "\n The reformulated nlp problem has a feasible solution. \n"
-        oad.ref_nlp_solution  = JuMP.value.(oad.ref_nlp_x)
-        nlp_objval                = JuMP.objective_value(oad.ref_nlp_model)
+        oad.milp_sol_available && (oad.ref_nlp_solution  = JuMP.value.(vars))
+        !oad.milp_sol_available && (oad.ref_nlp_solution  = JuMP.value.(oad.ref_nlp_x))
+        nlp_objval            = JuMP.objective_value(oad.ref_nlp_model)
         #update objval
-        if nlp_objval < oad.obj_val            #considering the problem is a min sense problem
+        if (nlp_objval < oad.obj_val) && oad.milp_sol_available            #considering the problem is a min sense problem
             oad.obj_val       = nlp_objval
             oad.incumbent     = oad.ref_nlp_solution
             oad.new_incumbent = true
@@ -33,7 +37,7 @@ function solve_ref_nlp_model(model::MOI.AbstractOptimizer, oad::OAdata)
         if isfinite(oad.obj_val) && isfinite(oad.obj_bound)
             oad.obj_gap = (oad.obj_val - oad.obj_bound)/(abs(oad.obj_val) + 1e-5)
         end
-    else
+    elseif (JuMP.termination_status(oad.ref_nlp_model) == MOI.INFEASIBLE) || (JuMP.termination_status(oad.ref_nlp_model) == MOI.DUAL_INFEASIBLE) || (JuMP.termination_status(oad.ref_nlp_model) == MOI.LOCALLY_INFEASIBLE) || (JuMP.termination_status(oad.ref_nlp_model) == MOI.INFEASIBLE_OR_UNBOUNDED)
         @info "\n The reformulated nlp problem is infeasible. \n"
         oad.nlp_infeasible = true
     end
@@ -46,19 +50,19 @@ function solve_ref_mip_model(model::MOI.AbstractOptimizer, oad::OAdata)
 """
 function solve_ref_mip_model(model::MOI.AbstractOptimizer, oad::OAdata)
     JuMP.set_optimizer(oad.ref_mip_model, model.options.mip_solver)
-    JuMP.optimize!(oad.ref_mip_model)
+    JuMP.optimize!(oad.ref_mip_model);
 
-    if JuMP.termination_status(oad.ref_mip_model) == :OPTIMAL || JuMP.termination_status(oad.ref_mip_model) == :LOCALLY_SOLVED
+    if (JuMP.termination_status(oad.ref_mip_model) ==  MOI.OPTIMAL) || (JuMP.termination_status(oad.ref_mip_model) ==  MOI.LOCALLY_SOLVED)
         @info "\n The reformulated mip problem has a feasible solution. \n"
-        oad.milp_sol_available = true
-        oad.prev_ref_mip_solution = oad.ref_mip_solution
+        (oad.milp_sol_available == true) && (oad.prev_ref_mip_solution = oad.ref_mip_solution)
+        (oad.milp_sol_available == false) && (oad.milp_sol_available = true)
         oad.ref_mip_solution = JuMP.value.(oad.ref_mip_x)
         # update bestbound
         mipobjbound = JuMP.objective_bound(oad.ref_mip_model)
         if isfinite(mipobjbound) && (mipobjbound > oad.obj_bound)
             oad.obj_bound = mipobjbound
         end
-    else
+    else (JuMP.termination_status(oad.ref_mip_model) == MOI.INFEASIBLE) || (JuMP.termination_status(oad.ref_mip_model) == MOI.DUAL_INFEASIBLE) || (JuMP.termination_status(oad.ref_mip_model) == MOI.LOCALLY_INFEASIBLE) || (JuMP.termination_status(oad.ref_mip_model) == MOI.INFEASIBLE_OR_UNBOUNDED)
         @info "\n The reformulated mip problem is infeasible. \n"
         oad.mip_infeasible = true
     end
@@ -69,23 +73,23 @@ end
 function solve_ref_feasibility_model(model::MOI.AbstractOptimizer, oa_data::OAdata)
     This function solves reformulated feasibility problem.
 """
-function solve_ref_feasibility_model(model::MOI.AbstractOptimizer, oa_data::OAdata)
+function solve_ref_feasibility_model(model::MOI.AbstractOptimizer, oad::OAdata)
     #fix int variables
-    if oa_data.milp_sol_available
-        for i in 1:oa_data.ref_num_var
-            if (oa_data.ref_var_type[i] == :Int) || (oa_data.ref_var_type[i] ==:Bin)
-                JuMP.set_lower_bound(oa_data.ref_feasibility_x[i], oa_data.ref_mip_solution[i])
-                JuMP.set_upper_bound(oa_data.ref_feasibility_x[i], oa_data.ref_mip_solution[i])
+    if oad.milp_sol_available
+        for i in 1:oad.ref_num_var
+            if (oad.ref_var_type[i] == :Int) || (oad.ref_var_type[i] ==:Bin)
+                JuMP.set_lower_bound(oad.ref_feasibility_x[i], oad.ref_mip_solution[i])
+                JuMP.set_upper_bound(oad.ref_feasibility_x[i], oad.ref_mip_solution[i])
             end
         end
     end
     #set optimizer and solve
-    JuMP.set_optimizer(oa_data.ref_feasibility_model, model.options.nlp_solver)
-    JuMP.optimize!(oa_data.ref_feasibility_model)
+    JuMP.set_optimizer(oad.ref_feasibility_model, model.options.nlp_solver)
+    JuMP.optimize!(oad.ref_feasibility_model);
 
-    if JuMP.termination_status(oa_data.ref_feasibility_model) == :OPTIMAL || JuMP.termination_status(oa_data.ref_feasibility_model) == :LOCALLY_SOLVED
+    if (JuMP.termination_status(oad.ref_feasibility_model) == MOI.OPTIMAL) || (JuMP.termination_status(oad.ref_feasibility_model) == MOI.LOCALLY_SOLVED)
         @info "\n The reformulated feasibility problem has a feasible solution. \n"
-        oa_data.ref_feasibility_solution = JuMP.value.(oa_data.ref_feasibility_x)
+        oad.ref_feasibility_solution = JuMP.value.(oad.ref_feasibility_x)
     else
         @error "\n The reformulated feasibility problem is infeasible. \n"
     end
@@ -121,14 +125,15 @@ function oa_algorithm(model::MOI.AbstractOptimizer, op::HybridProblem)
 """
 function oa_algorithm(model::MOI.AbstractOptimizer, op::OriginalProblem)
     #create linear approximation (milp) model from the original problem
-    oad=OAdata()
-    init_oad(op, oad)
+    oad = OAdata()
+    init_oa_data(op, oad)
     construct_linear_model(model, op, oad)
     add_oa_cut(model, op, oad)
     #create nlp model from the original problem
     construct_nlp_model(model, op, oad)
     #create reformulated nlp model from the original problem
     construct_ref_nlp_model(model, op, oad)
+    reserve_ref_linear_constraints(oad)
     #create linear approximation (milp) model from the reformulated nlp model
     construct_ref_mip_model(model, op, oad)
     #create reformulated feasibility problem
@@ -136,16 +141,16 @@ function oa_algorithm(model::MOI.AbstractOptimizer, op::OriginalProblem)
     #start of outer approximation algoirthm
     start_time = time()
     oad.oa_started = true
-    duration = 1000 #this could be provided as parameter
+    duration = 10000 #this could be provided as parameter
     while (time() - start_time) < duration
         oad.oa_iter += 1
         #solve reformulated nlp problem
-        solve_ref_nlp_model(model, oad)
+        solve_ref_nlp_model(model, oad);
         oad.nlp_infeasible && solve_ref_feasibility_model(model, oad)
         #update milp master problem
         add_ref_oa_cut(model, oad)
         #solve milp problem
-        solve_ref_mip_model(model, oad)
+        solve_ref_mip_model(model, oad);
         #check termination conditions
         check_termination_condition(oad)
         if (oad.oa_status == :Optimal) || (oad.oa_status == :Suboptimal) || (oad.oa_status == :FailedOA)
@@ -154,7 +159,12 @@ function oa_algorithm(model::MOI.AbstractOptimizer, op::OriginalProblem)
     end
     end_time = time()
     oad.total_time = (end_time-start_time)
-    @info "The outer approximation algorithm has been terminated. \n"
-    print("\n The objective value is :", oad.obj_val)
-    print("\n The solution is :", oad.incumbent)
+    print("\n The outer approximation algorithm has been terminated. \n")
+    print("\n The solution status :", oad.oa_status, "\n")
+    print("\n The number of oa iterations : ", oad.oa_iter, "\n")
+    print("\n The solution time : ", oad.total_time, " secs \n")
+    print("\n The objective value :", oad.obj_val, "\n")
+    print("\n The objective gap :", oad.obj_gap, "\n")
+    print("\n The solution :", oad.incumbent, "\n")
+    print("\n The mip solution :", oad.ref_mip_solution, "\n")
 end
